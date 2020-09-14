5e889dc4cc6acb406aee33a4862218fea21bab03
Anil Panada <anil.k.panada1908@gmail.cmm>
2020-09-14 08:02:10 +0530

    day1 finished

---
+++ src/Home.js
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product.js";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+        <div className="home_row">
+          <Product
+            id="12345678"
+            title="The lean startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product
+            id="12345670"
+            title="Kenwood mix Stand Mixer for baking, Stylish Kitchen Mixer with K-beater,
+                        Dough hook and whisks, 5 Litre Glass Bowl"
+            price={239.0}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+            rating={4}
+          />
+        </div>
+        <div className="home_row">
+          <Product
+            id="12345671"
+            title="Samsung LC494RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={199.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL.AC_SX466_.jpg"
+            rating={3}
+          />
+          <Product
+            id="12345672"
+            title="Amazon Echo (3rd generation) | Smartt speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+            rating={5}
+          />
+          <Product
+            id="12345673"
+            title="New Apple iPad Pro (12.9-inch, wifi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+            rating={4}
+          />
+        </div>
+        <div className="home_row">
+          <Product
+            id="02345670"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={1094.98}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+            rating={4}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCLpvZdl8Kxy5oothBEUDKV0fSXLulaZ9o",
+    authDomain: "clone-df52a.firebaseapp.com",
+    databaseURL: "https://clone-df52a.firebaseio.com",
+    projectId: "clone-df52a",
+    storageBucket: "clone-df52a.appspot.com",
+    messagingSenderId: "128588153840",
+    appId: "1:128588153840:web:84e6dfa335cdcd2d23d335",
+    measurementId: "G-48TH93MQN5"
+  };
